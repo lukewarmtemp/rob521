776a45892d786ec5ffc1f5d74c7e5bf04a6c7fb1
Mibby123 <rohan48b@gmail.com>
2025-01-21 14:22:25 -0500

    Mac Docker Build File

---
+++ mac_docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker run --platform linux/amd64 -it --user ros --network=host --ipc=host \
+-v /tmp/.X11-unix:/tmp/.X11-unix:rw --env=DISPLAY \
+-e LIBGL_ALWAYS_SOFTWARE=1 \
+-v ./catkin_ws:/catkin_ws lukewarmtemp/rob521:lab01

beeb9468a758d1d4f910fa7854d157064edbc1f3
Luke Yang <luke.jia.yang@gmail.com>
2025-02-27 17:12:00 -0500

    fixed gitignore

--- catkin_ws/build/lab2_v3/lab2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /catkin_ws/src/lab2_v3/lab2
-# Build directory: /catkin_ws/build/lab2_v3/lab2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

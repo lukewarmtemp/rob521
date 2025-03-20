03973792e48a1bd01124af960cac6448296d636d
Luke Yang <luke.jia.yang@gmail.com>
2025-03-20 19:32:16 -0400

    fixed docker file

--- docker.sh
+++ docker.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Allow docker to connect to X server (run outside docker)
+xhost +local:docker
+
 docker run --platform linux/amd64 -it --user ros --network=host --ipc=host \
 -v /tmp/.X11-unix:/tmp/.X11-unix:rw --env=DISPLAY \
 -e LIBGL_ALWAYS_SOFTWARE=1 \
+-e QT_X11_NO_MITSHM=1 \
 -v ./catkin_ws:/catkin_ws lukewarmtemp/rob521:lab01
